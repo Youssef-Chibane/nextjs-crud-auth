e0e57dbbe501668e8edc6f3fee3d8155bb267f9f
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-26 13:28:47 +0100

    dynamic metadata for plant page

--- app/plants/[slug]/page.tsx
+++ app/plants/[slug]/page.tsx
@@ -1,9 +1,30 @@
 import { getPlantById } from "@/app/actions/plants-action";
 import PlantCard from "./PlantCard";
+import { stackServerApp } from "@/stack";
+import { SignIn } from "@stackframe/stack";
 
-async function PlantPage({ params }: { params: { slug: string } }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  // Extract the id from the slug by splitting on the delimiter
   const [id] = params.slug.split("--");
   const plant = await getPlantById(id);
+  return {
+    title: plant ? plant.name : "Plant Details",
+    description: plant ? plant.description : "Plant details page",
+  };
+}
+
+async function PlantPage({ params }: { params: { slug: string } }) {
+  const user = await stackServerApp.getUser();
+  const [id] = params.slug.split("--");
+  const plant = await getPlantById(id);
+
+  if (!user) {
+    return <SignIn />;
+  }
 
   return (
     <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
