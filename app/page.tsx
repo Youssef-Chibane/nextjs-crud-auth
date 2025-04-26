8d6d2adb86e89e88b699678436775c4c0ac48a27
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-26 08:29:16 +0100

    Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Home Page</h1>
+      <Hero />
     </div>
   );
 }
