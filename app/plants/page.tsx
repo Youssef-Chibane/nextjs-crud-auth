0b311ff85c6ad7cdbd39c243a6aac624ed92753b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-26 12:45:17 +0100

    passed plants prop to InventoryTable

--- app/plants/page.tsx
+++ app/plants/page.tsx
@@ -1,16 +1,18 @@
 import InventoryTable from "@/components/InventoryTable";
 import { stackServerApp } from "@/stack";
 import { SignIn } from "@stackframe/stack";
+import { getPlants } from "../actions/plants-action";
 
 export default async function PlantsPage() {
   const user = await stackServerApp.getUser();
   const app = stackServerApp.urls;
+  const plants = await getPlants();
   return (
     <div>
       {user ? (
         <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
           <div className="lg:col-span-full">
-            <InventoryTable />
+            <InventoryTable plants={plants} />
           </div>
         </div>
       ) : (
