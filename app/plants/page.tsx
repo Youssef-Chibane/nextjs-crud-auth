02c4dbd1cf66a52462f11521bba5322b70923cac
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-26 07:03:00 +0100

    Home&Plants pages

---
+++ app/plants/page.tsx
@@ -0,0 +1,7 @@
+export default function PlantsPage() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Plants Page</h1>
+    </div>
+  );
+}
