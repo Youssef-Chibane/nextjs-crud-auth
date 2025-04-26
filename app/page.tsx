ca474929f01d158c0e67902b07f0a1ee2c2a93f1
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-26 10:52:32 +0100

    added Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div>
       <Hero />
     </div>
   );
